7051606222ca844f2c02a3ac94c82fbfa3e45ecc
MerveKaragoz1 <mervekaragoz13@gmail.com>
2021-05-15 16:51:23 +0300

    v2 -merve

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,18 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using yazilimYapimi.Models;
 
 namespace yazilimYapimi.Controllers
 {
+    
     public class HomeController : Controller
     {
-       
+        yazilimyapimiEntities db = new yazilimyapimiEntities();
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Register(string ad, string soyad, string kad, string tc, string sifre, string tel, string mail, string adres, bool yetki = true)
+        {
+            kullanici kullanici = new kullanici()
+            {
+                ad = ad,
+                soyad = soyad,
+                kad = kad,
+                sifre = sifre,
+                tel = tel,
+                mail = mail,
+                adres = adres,
+                yetki = yetki,
+                tc = tc,
+                bakiye = 0
+            };
+
+            db.kullanici.Add(kullanici);
+            db.SaveChanges();
+
+
+            return RedirectToAction("Index");
+
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -25,6 +50,27 @@ namespace yazilimYapimi.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult UrunEkle()
+        {
+            ViewBag.Message = "Ürün ekleme sayfası.";
+
+            return View();
+        }
+
+        public ActionResult BakiyeEkle()
+        {
+            ViewBag.Message = "Bakiye ekleme.";
+
+            return View();
+        }
+
+        public ActionResult Urunler()
+        {
+            ViewBag.Message = "Urun listeleme.";
 
             return View();
         }
