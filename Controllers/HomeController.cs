42402ec147a5f2d736b971af76101367dc0766e2
KUBİLAY GÖKAY <192804009@ogr.cbu.edu.tr>
2021-05-17 08:01:09 +0300

    v6

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,35 +16,22 @@ namespace yazilimYapimi.Controllers
 
         List<obakiye> obakiyes = new List<obakiye>();
 
+        List<ourunler> ourunlers = new List<ourunler>();
+
         List<bakiyesahibi> bakiyesahibis = new List<bakiyesahibi>();
+
+        List<urunsahibi> urunsahibis = new List<urunsahibi>();
 
         //sayfa action resultları
         public ActionResult Index()
         {
-            obakiyes = db.obakiye.ToList();
-            foreach (var item in obakiyes)
-            {
-                bakiyesahibi bakiyesahibi = new bakiyesahibi();
-                bakiyesahibi.kid = item.k_id;
-                bakiyesahibi.para = Convert.ToInt32(item.bakiye);
-                bakiyesahibi.kullanici = db.kullanici.FirstOrDefault(x => x.k_id == item.k_id).kad;
-                bakiyesahibis.Add(bakiyesahibi);
-            }
-            ViewBag.bakiye = bakiyesahibis;
+            onayListele();
             return View();
+
         }
         public ActionResult UrunEkle()
         {
-            obakiyes = db.obakiye.ToList();
-            foreach (var item in obakiyes)
-            {
-                bakiyesahibi bakiyesahibi = new bakiyesahibi();
-                bakiyesahibi.kid = item.k_id;
-                bakiyesahibi.para = Convert.ToInt32(item.bakiye);
-                bakiyesahibi.kullanici = db.kullanici.FirstOrDefault(x => x.k_id == item.k_id).kad;
-                bakiyesahibis.Add(bakiyesahibi);
-            }
-            ViewBag.bakiye = bakiyesahibis;
+            onayListele();
             ViewBag.Message = "Ürün ekleme sayfası.";
 
             return View();
@@ -52,16 +39,8 @@ namespace yazilimYapimi.Controllers
 
         public ActionResult BakiyeEkle()
         {
-            obakiyes = db.obakiye.ToList();
-            foreach (var item in obakiyes)
-            {
-                bakiyesahibi bakiyesahibi = new bakiyesahibi();
-                bakiyesahibi.kid = item.k_id;
-                bakiyesahibi.para = Convert.ToInt32(item.bakiye);
-                bakiyesahibi.kullanici = db.kullanici.FirstOrDefault(x => x.k_id == item.k_id).kad;
-                bakiyesahibis.Add(bakiyesahibi);
-            }
-            ViewBag.bakiye = bakiyesahibis;
+            onayListele();
+
             ViewBag.Message = "Bakiye ekleme.";
 
             return View();
@@ -69,6 +48,46 @@ namespace yazilimYapimi.Controllers
 
         public ActionResult Urunler()
         {
+            onayListele();
+            ViewBag.Message = "Urun listeleme.";
+
+            List<urunler> urunlers = new List<urunler>();
+            urunlers = db.urunler.ToList();
+            List<urun> uruns = new List<urun>();
+            foreach (var item in urunlers)
+            {
+                int kullanici_id = Convert.ToInt32(item.k_id);
+                urun urun = new urun() {
+                    urunadi = item.urun,
+                    fiyat = Convert.ToInt32(item.fiyat),
+                    k_id = kullanici_id,
+                    miktar = Convert.ToInt32(item.miktar),
+                    u_id = Convert.ToInt32(item.u_id),
+                    satici = db.kullanici.FirstOrDefault(x => x.k_id == kullanici_id).kad
+                };
+                uruns.Add(urun);
+            }
+            ViewBag.urunList = uruns;
+
+            return View();
+        }
+
+        public void onayListele()
+        {
+            ourunlers = db.ourunler.ToList();
+            foreach (var item in ourunlers)
+            {
+                urunsahibi urunSahibi = new urunsahibi();
+                urunSahibi.fiyat = Convert.ToInt32(item.fiyat);
+                urunSahibi.k_id = Convert.ToInt32(item.k_id);
+                urunSahibi.miktar = Convert.ToInt32(item.miktar);
+                urunSahibi.urun = item.urun;
+                urunSahibi.urun_id = Convert.ToInt32(item.urun_id);
+                urunSahibi.kullanici = db.kullanici.FirstOrDefault(x => x.k_id == item.k_id).kad;
+                urunsahibis.Add(urunSahibi);
+            }
+            ViewBag.urun = urunsahibis;
+
             obakiyes = db.obakiye.ToList();
             foreach (var item in obakiyes)
             {
@@ -79,11 +98,7 @@ namespace yazilimYapimi.Controllers
                 bakiyesahibis.Add(bakiyesahibi);
             }
             ViewBag.bakiye = bakiyesahibis;
-            ViewBag.Message = "Urun listeleme.";
-
-            return View();
         }
-
 
 
         //Kayıt işlemi
@@ -173,12 +188,11 @@ namespace yazilimYapimi.Controllers
         public ActionResult BakiyeOnay(string id, string onay, string bakiye = "0")
         {
             int kid = Convert.ToInt32(id);
-            int para = Convert.ToInt32(bakiye);
 
             if (onay == "1")
             {
                 var user = db.kullanici.FirstOrDefault(x => x.k_id == kid);
-                user.bakiye += para;
+                user.bakiye += Convert.ToInt32(bakiye);
                 var b = db.obakiye.FirstOrDefault(x => x.k_id == kid);
                 db.obakiye.Remove(b);
                 db.SaveChanges();
@@ -194,7 +208,7 @@ namespace yazilimYapimi.Controllers
         }
 
         //urun ekle -urun onay
-        public ActionResult urunEkleme(string urun, string adet,string fiyat)
+        public ActionResult urunEkleme(string urun, string adet, string fiyat)
         {
             ourunler ourunler = new ourunler()
             {
@@ -202,10 +216,39 @@ namespace yazilimYapimi.Controllers
                 miktar = Convert.ToInt32(adet),
                 fiyat = Convert.ToInt32(fiyat),
                 urun = urun
-            } ;
+            };
             db.ourunler.Add(ourunler);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult UrunOnay(string urun_id, string miktar, string k_id, string urun, string fiyat, string onay)
+        {
+            int u_id = Convert.ToInt32(urun_id);
+            if (onay == "1")
+            {
+                urunler urunler = new urunler()
+                {
+                    u_id = u_id,
+                    k_id = Convert.ToInt32(k_id),
+                    miktar = Convert.ToInt32(miktar),
+                    fiyat = Convert.ToInt32(fiyat),
+                    urun = urun
+                };
+                db.urunler.Add(urunler);
+
+                var b = db.ourunler.FirstOrDefault(x => x.urun_id == u_id);
+                db.ourunler.Remove(b);
+                db.SaveChanges();
+            }
+            else
+            {
+                var b = db.ourunler.FirstOrDefault(x => x.urun_id == u_id);
+                db.ourunler.Remove(b);
+                db.SaveChanges();
+            }
+            return Redirect("/");
+        }
+
+
     }
 }
---
+++ classes/urun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yazilimYapimi.classes
+{
+    public class urun
+    {
+        public int u_id { get; set; }
+        public int k_id { get; set; }
+        public string satici { get; set; }
+        public int miktar { get; set; }
+        public int fiyat { get; set; }
+        public string urunadi { get; set; }
+    }
+}
