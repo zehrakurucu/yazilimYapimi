b56c7540964d98654817da58a6c16eabd7f341e0
MerveKaragoz1 <mervekaragoz13@gmail.com>
2021-05-17 04:18:23 +0300

    v5

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,7 +9,7 @@ using yazilimYapimi.Models;
 
 namespace yazilimYapimi.Controllers
 {
-    
+
     public class HomeController : Controller
     {
         yazilimyapimiEntities db = new yazilimyapimiEntities();
@@ -18,71 +18,10 @@ namespace yazilimYapimi.Controllers
 
         List<bakiyesahibi> bakiyesahibis = new List<bakiyesahibi>();
 
+        //sayfa action resultları
         public ActionResult Index()
         {
             obakiyes = db.obakiye.ToList();
-            foreach (var item in obakiyes)
-            {
-                bakiyesahibi bakiyesahibi = new bakiyesahibi();
-                bakiyesahibi.kid = item.k_id;
-                bakiyesahibi.para =Convert.ToInt32(item.bakiye);
-                bakiyesahibi.kullanici = db.kullanici.FirstOrDefault(x => x.k_id == item.k_id).kad;
-                bakiyesahibis.Add(bakiyesahibi);
-            }
-             ViewBag.bakiye=bakiyesahibis;
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Register(string ad, string soyad, string kad, string tc, string sifre, string tel, string mail, string adres, bool yetki = true)
-        {
-            kullanici kullanici = new kullanici()
-            {
-                ad = ad,
-                soyad = soyad,
-                kad = kad,
-                sifre = sifre,
-                tel = tel,
-                mail = mail,
-                adres = adres,
-                yetki = yetki,
-                tc = tc,
-                bakiye = 0
-            };
-
-            db.kullanici.Add(kullanici);
-            db.SaveChanges();
-
-
-            return RedirectToAction("Index");
-
-        }
-
-        [HttpPost]
-        public ActionResult Login(string kad,string sifre)
-        {
-            if (kad=="admin"&&sifre=="123")
-            {
-                Session["yetki"] = 2;
-                return RedirectToAction("Urunler");
-            }
-
-            var kullanici = db.kullanici.FirstOrDefault(x => x.kad == kad && x.sifre == sifre);
-            if (kullanici!=null)
-            {
-                Session["yetki"] = kullanici.yetki;
-                Session["k_id"] = kullanici.k_id;
-                return RedirectToAction("Urunler");
-            }
-            else
-            {
-                ViewBag.hata = "hatalı giriş";
-                return RedirectToAction("Index");
-            }
-        }
-        [HttpPost]
-        public ActionResult bakiyeEkle(string bakiye)
-        {
             foreach (var item in obakiyes)
             {
                 bakiyesahibi bakiyesahibi = new bakiyesahibi();
@@ -92,41 +31,8 @@ namespace yazilimYapimi.Controllers
                 bakiyesahibis.Add(bakiyesahibi);
             }
             ViewBag.bakiye = bakiyesahibis;
-
-            obakiye obakiye = new obakiye()
-            {
-                bakiye = Convert.ToInt32(bakiye),
-                k_id=Convert.ToInt32(Session["k_id"])
-            };
-            db.obakiye.Add(obakiye);
-            db.SaveChanges();
-            return RedirectToAction("Urunler");
-        }
-
-        
-
-
-
-        public ActionResult Cikis()
-        {
-            Session.Clear();
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult UrunEkle()
         {
             obakiyes = db.obakiye.ToList();
@@ -178,12 +84,98 @@ namespace yazilimYapimi.Controllers
             return View();
         }
 
-        public ActionResult BakiyeOnay(string id,string onay,string bakiye="0")
+
+
+        //Kayıt işlemi
+
+        [HttpPost]
+        public ActionResult Register(string ad, string soyad, string kad, string tc, string sifre, string tel, string mail, string adres, bool yetki = true)
+        {
+            kullanici kullanici = new kullanici()
+            {
+                ad = ad,
+                soyad = soyad,
+                kad = kad,
+                sifre = sifre,
+                tel = tel,
+                mail = mail,
+                adres = adres,
+                yetki = yetki,
+                tc = tc,
+                bakiye = 0
+            };
+
+            db.kullanici.Add(kullanici);
+            db.SaveChanges();
+
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        public ActionResult Login(string kad, string sifre)
+        {
+            if (kad == "admin" && sifre == "123")
+            {
+                Session["yetki"] = 2;
+                return RedirectToAction("Urunler");
+            }
+
+            var kullanici = db.kullanici.FirstOrDefault(x => x.kad == kad && x.sifre == sifre);
+            if (kullanici != null)
+            {
+                Session["yetki"] = kullanici.yetki;
+                Session["k_id"] = kullanici.k_id;
+                Session["para"] = kullanici.bakiye;
+
+
+
+                return RedirectToAction("Urunler");
+            }
+            else
+            {
+                ViewBag.hata = "hatalı giriş";
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Cikis()
+        {
+            Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+
+        //bakiye ekle- bakiye admin onay
+
+        [HttpPost]
+        public ActionResult bakiyeEkle(string bakiye)
+        {
+            foreach (var item in obakiyes)
+            {
+                bakiyesahibi bakiyesahibi = new bakiyesahibi();
+                bakiyesahibi.kid = item.k_id;
+                bakiyesahibi.para = Convert.ToInt32(item.bakiye);
+                bakiyesahibi.kullanici = db.kullanici.FirstOrDefault(x => x.k_id == item.k_id).kad;
+                bakiyesahibis.Add(bakiyesahibi);
+            }
+            ViewBag.bakiye = bakiyesahibis;
+
+            obakiye obakiye = new obakiye()
+            {
+                bakiye = Convert.ToInt32(bakiye),
+                k_id = Convert.ToInt32(Session["k_id"])
+            };
+            db.obakiye.Add(obakiye);
+            db.SaveChanges();
+            return RedirectToAction("Urunler");
+        }
+        public ActionResult BakiyeOnay(string id, string onay, string bakiye = "0")
         {
             int kid = Convert.ToInt32(id);
             int para = Convert.ToInt32(bakiye);
 
-            if (onay=="1")
+            if (onay == "1")
             {
                 var user = db.kullanici.FirstOrDefault(x => x.k_id == kid);
                 user.bakiye += para;
@@ -199,6 +191,21 @@ namespace yazilimYapimi.Controllers
             }
 
             return Redirect("/");
+        }
+
+        //urun ekle -urun onay
+        public ActionResult urunEkleme(string urun, string adet,string fiyat)
+        {
+            ourunler ourunler = new ourunler()
+            {
+                k_id = Convert.ToInt32(Session["k_id"]),
+                miktar = Convert.ToInt32(adet),
+                fiyat = Convert.ToInt32(fiyat),
+                urun = urun
+            } ;
+            db.ourunler.Add(ourunler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
